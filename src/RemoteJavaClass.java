ccc16e128263eef19055d94401506d9738899b2c
“Yelbaygit config --global user.email cnntylby@gmail.comgit config --global credential.username Yelbaygit config --global credential.password Cennet1983 <cnntylby@gmail.comgit config --global user.name Yelbay>
2022-04-02 12:05:36 -0400

    commiting new classes

--- src/RemoteJavaClass.java
+++ src/RemoteJavaClass.java
@@ -1,9 +1,3 @@
-<<<<<<< HEAD:src/RemoteJavaClass.Java
 public class RemoteJavaClass {
     //This will also  have some code
 }
-=======
-public class RemoteJavaClass{
-  //This will also  have some code
-}
->>>>>>> 3b5180fe95de519340d10cdc84f8252f0c312284:src/RemoteJavaClass.java
